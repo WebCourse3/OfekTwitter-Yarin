6ab35b18dc3ce6abe1bb73a6d0a59e03cc18b201
yarinr <y3arin@gmail.com>
2017-10-30 09:17:12 +0200

    slight refactor of test functionality

--- test/test.js
+++ test/test.js
@@ -1,31 +1,35 @@
 (function () {
-    function test_group(name, test_group_function) {
+
+    function createTestGroupElement(name) {
         const testGroupElement = document.createElement('div');
         testGroupElement.setAttribute('class', 'test-group');
         testGroupElement.innerHTML = `<h3>${name}</h3>`;
+        return testGroupElement;
+    }
+
+    function test_group(name, test_group_function) {
+        const testGroupElement = createTestGroupElement(name);
         document.getElementById('tests-section').appendChild(testGroupElement);
 
-        let passed = true;
+        let allTestsPassed = true;
 
         assert = (value, name) => { 
-            passed = passed && value;
             let elementClass = value ? 'passed' : 'failed';
-    
+            allTestsPassed = allTestsPassed && value;
+            
             const elementString = `
                 <div class="test ${elementClass}">
                 ${name} - ${elementClass}
                 </div>
             `
-    
             testGroupElement.innerHTML += elementString;
         }
 
         test_group_function();
 
-        testGroupElement.className += passed ? ' passed' : ' failed';
+        testGroupElement.className += allTestsPassed ? ' passed' : ' failed';
     }
 
-    
     test_group('shit', function() {
         assert(true, 'Test1');
         assert(false, 'Test2');
